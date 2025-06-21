6c15a0a1c84808a6575feaac8b19f55603b5a435
RChaubey16 <ruturajchaubey16@gmail.com>
2025-06-21 10:54:37 +0530

    ui: add grand-prixx card component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,33 @@
-import StatsCard from "@/components/StatsCard";
+import StatsCard from "@/components/StatsCard";;
+import GrandPrixCard from "@/components/GrandPrixCard";
 
 export default async function Home() {
   return (
-    <div className="grid grid-cols-1 gap-4 ">
-      <StatsCard
-        cardLabel="Leading Constructor"
-        cardTitle="McLaren Racing"
-        points={204}
-        logo="/teams/mclaren-logo.svg"
-        logoType="team"
-      />
-      <StatsCard
-        cardLabel="Leading Driver"
-        cardTitle="Max Verstappen"
-        points={204}
-        logo="/drivers/max-verstappen.svg"
-        logoType="driver"
-      />
-    </div>
+    <>
+      <div className="grid grid-cols-1 gap-4 ">
+        <StatsCard
+          cardLabel="Leading Constructor"
+          cardTitle="McLaren Racing"
+          points={204}
+          logo="/teams/mclaren-logo.svg"
+          logoType="team"
+        />
+        <StatsCard
+          cardLabel="Leading Driver"
+          cardTitle="Max Verstappen"
+          points={204}
+          logo="/drivers/max-verstappen.svg"
+          logoType="driver"
+        />
+      </div>
+
+      <div className="w-[665px]">
+        <GrandPrixCard
+          gpName="Austrian Grand Prix"
+          gpDate="May 26, 2025"
+          gpTime="5 days, 12 hours"
+        />
+      </div>
+    </>
   );
 }
---
+++ components/GrandPrixCard.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+
+type GrandPrixCardProps = {
+  gpName: string;
+  gpDate: string;
+  gpTime: string;
+};
+
+const GrandPrixCard = ({
+  gpName = "Austrian Grand Prix",
+  gpDate = "June 27, 2025",
+  gpTime = "09 days, 22 hours",
+}: GrandPrixCardProps) => {
+  return (
+    <div className="grand-prix-card">
+      <div className="flex flex-col gap-6 justify-between">
+        <div className="space-y-1">
+          <p className="title-text text-2xl">{gpName}</p>
+          <p className="label-text text-sm">{gpDate}</p>
+        </div>
+        <p className="points-text">{gpTime}</p>
+      </div>
+
+      <div>
+        <Image
+          src={"/circuits/red-bull-ring.svg"}
+          alt={`logo`}
+          width={140}
+          height={40}
+        />
+      </div>
+      <div className="relative right-[-18]">
+        <Image
+          src={"/flags/austria-flag.png"}
+          alt={`logo`}
+          width={100}
+          height={40}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default GrandPrixCard;
